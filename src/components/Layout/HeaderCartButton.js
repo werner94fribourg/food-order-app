df0ba37d6ca56dbd3dfdde254265796d39be631b
werner94fribourg <werner94@hotmail.com>
2022-12-06 15:15:14 +0100

    Managing Cart & Modal State

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,7 +2,7 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.scss';
 
 const HeaderCartButton = props => (
-  <button className={classes.button}>
+  <button className={classes.button} onClick={props.onClick}>
     <span className={classes.icon}>
       <CartIcon />
     </span>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import classes from './Modal.module.scss';
 
 const Backdrop = props => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = props => {
@@ -19,7 +19,10 @@ const portalElement = document.querySelector('#overlays');
 const Modal = props => {
   return (
     <React.Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
