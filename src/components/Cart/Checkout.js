383adaf235625e270c36340e0fe4316b94bda938
werner94fribourg <werner94@hotmail.com>
2022-12-09 10:28:04 +0100

    Reading Form Values

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,31 +1,46 @@
+import { useRef } from 'react';
 import classes from './Checkout.module.scss';
 
 const Checkout = props => {
+  const nameInput = useRef();
+  const streetInput = useRef();
+  const postalCodeInput = useRef();
+  const cityInput = useRef();
+
   const confirmHandler = event => {
     event.preventDefault();
+
+    const enteredName = nameInput.current.value;
+    const enteredStreet = streetInput.current.value;
+    const enteredPostalCode = postalCodeInput.current.value;
+    const enteredCity = cityInput.current.value;
   };
   return (
     <form onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input id="name" type="text" />
+        <input id="name" type="text" ref={nameInput} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input id="street" type="text" />
+        <input id="street" type="text" ref={streetInput} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input id="postal" type="text" />
+        <input id="postal" type="text" ref={postalCodeInput} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input id="city" type="text" />
+        <input id="city" type="text" ref={cityInput} />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button type="submit">Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit} type="submit">
+          Confirm
+        </button>
+      </div>
     </form>
   );
 };
